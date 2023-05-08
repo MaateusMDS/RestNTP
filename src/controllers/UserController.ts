467305438364ad93df6de71de8dd95e8af5b4423
Mateus <maatmds04@gmail.com>
2023-05-08 08:53:37 -0300

    refactoring js to ts in usercontroller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,11 @@
+import {Request, Response} from 'express';
+import { prisma } from '../database';
+
 export default {
   async createUser(request: Request, response: Response){
     try {
-        const {name, email} = request.body;
-        const userExist = await prisma
+      const {name, email} = request.body;
+      const userExist = await prisma.user;
     } catch (err) {
       return response.json({message: err.message});
     }
