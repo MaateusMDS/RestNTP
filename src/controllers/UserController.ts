8ee07ca769448f7a9b293af0a68fa13460a84b1a
Mateus <maatmds04@gmail.com>
2023-05-08 09:09:32 -0300

    usercontroller now is working and adding data

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,33 @@
-import {Request, Response} from 'express';
+import {Request, Response, response} from 'express';
 import { prisma } from '../database';
 
 export default {
   async createUser(request: Request, response: Response){
     try {
       const {name, email} = request.body;
-      const userExist = await prisma.user;
+      const userExist = await prisma.user.findUnique({where: {email}});
+
+      if (userExist){
+        return response.json(
+          {
+            error: true,
+            message: 'Erro: Usuário já existe.'
+          }
+        );
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          name, email
+        }
+      });
+
+      return response.json({
+        error: false,
+        message: 'Sucesso! Usuário criado.',
+        user
+      });
+
     } catch (err) {
       return response.json({message: err.message});
     }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Express from 'express';
+import UserController from './controllers/UserController';
 
 const app = Express();
 app.use(Express.json());
@@ -7,6 +8,8 @@ const PORT= 8000;
 app.get('/', (request, response) => {
   return response.send({message: 'Hello World'});
 });
+
+app.post('/createUser', UserController.createUser);
 
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
